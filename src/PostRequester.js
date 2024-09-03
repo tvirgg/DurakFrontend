a16293c8a30804c00071912c7cd028398ed71c62
Esternit <92298236+Esternit@users.noreply.github.com>
2024-09-03 20:08:54 +0300

    + game creating

--- src/PostRequester.js
+++ src/PostRequester.js
@@ -3,7 +3,16 @@ import config from "./config";
 import ShowPopup from "./ShowPopup";
 const PostRequester = async (url, body) => {
   try {
-    await axios.post(config.url + url, body, { withCredentials: true });
+    await axios
+      .post(config.url + url, body, {
+        headers: {
+          "Access-Control-Expose-Headers": "X-Session",
+          "X-Session": localStorage.getItem("session_key"),
+        },
+      })
+      .then((res) => {
+        localStorage.setItem("session_key", res.headers.get("X-Session"));
+      });
   } catch (err) {
     if (err.response.data) {
       ShowPopup(err.response.data, "Error");
--- src/pages/lobbies.create.jsx
+++ src/pages/lobbies.create.jsx
@@ -1,43 +1,47 @@
-import React from 'react'
-import '../media/css/page/lobbies.create.css'
+import React from "react";
+import "../media/css/page/lobbies.create.css";
+// Import Swiper React components
+import IconEye from "../components/icons/eye";
 // import IconAlertCircle from "../components/icons/alertCircle";
-import IconArrowDegRight from '../components/icons/arrowDegRight'
-import IconRefresh from '../components/icons/refresh'
+import IconPlayWhite from "../components/icons/playWhite";
+import IconArrowDegRight from "../components/icons/arrowDegRight";
+import IconRefresh from "../components/icons/refresh";
+import IconAlertCircle from "../components/icons/alertCircle";
 // UI
-import ProgressBar from '../components/ui/progressBar'
-import IconEye from '../components/icons/eye'
+import ProgressBar from "../components/ui/progressBar";
+
 // data
-import LobbiesLayout from '../layouts/lobbies.layout'
+import LobbiesLayout from "../layouts/lobbies.layout";
 // -
-import axios from 'axios'
-import ShowPopup from '../ShowPopup'
-import config from '../config'
-import PostRequester from '../PostRequester'
-const bids = [0, 1, 10, 100, 500, 1000, 5000, 10000, 50000, 100000]
+import axios from "axios";
+import ShowPopup from "../ShowPopup";
+import config from "../config";
+import PostRequester from "../PostRequester";
+const bids = [0, 1, 10, 100, 500, 1000, 5000, 10000, 50000, 100000];
 
 const LobbiesCreate = () => {
-  const [playerAmount, setPlayerAmount] = React.useState(1)
-  const [bidCur, setBidCur] = React.useState('Free')
-  const [bidAmount, setBidAmount] = React.useState(0)
-  const [progress, setProgress] = React.useState(0)
-  const [name, setName] = React.useState('Anonim')
-  const [gameType, setGameType] = React.useState('CLASSIC')
+  const [playerAmount, setPlayerAmount] = React.useState(1);
+  const [bidCur, setBidCur] = React.useState("Free");
+  const [bidAmount, setBidAmount] = React.useState(0);
+  const [progress, setProgress] = React.useState(0);
+  const [name, setName] = React.useState("Anonim");
+  const [gameType, setGameType] = React.useState("CLASSIC");
 
   const createGame = async () => {
-    await PostRequester('/game/create', {
+    await PostRequester("/game/create", {
       fieldSize: (playerAmount + 1) * 6,
       type: gameType,
-      allowedShullerMoves: gameType === 'SHULLER' ? 1 : 0,
-      betAmount: bidCur === 'Free' ? 0 : bidAmount,
-      betType: bidCur === 'Free' ? 'usual' : bidCur,
-      name: name.length > 0 ? name : 'Anonimus',
-    })
-  }
+      allowedShullerMoves: gameType === "SHULLER" ? 1 : 0,
+      betAmount: bidCur === "Free" ? 0 : bidAmount,
+      betType: bidCur === "Free" ? "usual" : bidCur,
+      name: name.length > 0 ? name : "Anonimus",
+    });
+  };
 
   const bidChanger = (value) => {
-    setBidAmount(bids[value])
-    setProgress(value)
-  }
+    setBidAmount(bids[value]);
+    setProgress(value);
+  };
   return (
     <LobbiesLayout>
       <div className="create_window">
@@ -46,126 +50,88 @@ const LobbiesCreate = () => {
             type="text"
             placeholder="Enter lobby name..."
             name="lobby_name"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         {/* players / progress-bar */}
         <p>Choose number of players</p>
         <div className="players_number">
-          <input
-            type="radio"
-            id="pn1"
-            name="players_number"
-          />
-          <label htmlFor="pn1">1</label>
-          <input
-            type="radio"
-            id="pn2"
-            name="players_number"
-          />
-          <label htmlFor="pn2">2</label>
-          <input
-            type="radio"
-            id="pn3"
-            name="players_number"
-          />
-          <label htmlFor="pn3">3</label>
-          <input
-            type="radio"
-            id="pn4"
-            name="players_number"
-          />
-          <label htmlFor="pn4">4</label>
-          <input
-            type="radio"
-            id="pn5"
-            name="players_number"
-          />
-          <label htmlFor="pn5">5</label>
-          <input
-            type="radio"
-            id="pn6"
-            name="players_number"
-          />
-          <label htmlFor="pn6">6</label>
+          <input type="radio" id="pn1" name="players_number" />
+          <label htmlFor="pn1" onClick={() => setPlayerAmount(1)}>
+            1
+          </label>
+          <input type="radio" id="pn2" name="players_number" />
+          <label htmlFor="pn2" onClick={() => setPlayerAmount(2)}>
+            2
+          </label>
+          <input type="radio" id="pn3" name="players_number" />
+          <label htmlFor="pn3" onClick={() => setPlayerAmount(3)}>
+            3
+          </label>
+          <input type="radio" id="pn4" name="players_number" />
+          <label htmlFor="pn4" onClick={() => setPlayerAmount(4)}>
+            4
+          </label>
+          <input type="radio" id="pn5" name="players_number" />
+          <label htmlFor="pn5" onClick={() => setPlayerAmount(5)}>
+            5
+          </label>
+          <input type="radio" id="pn6" name="players_number" />
+          <label htmlFor="pn6" onClick={() => setPlayerAmount(6)}>
+            6
+          </label>
         </div>
         {/* bid */}
         <p>Bids currency</p>
         <div className="bid">
-          <input
-            type="radio"
-            id="cbid1"
-            name="bid"
-          />
-          <label htmlFor="cbid1">Free</label>
-          <input
-            type="radio"
-            id="cbid2"
-            name="bid"
-          />
-          <label htmlFor="cbid2">DUR</label>
-          <input
-            type="radio"
-            id="cbid3"
-            name="bid"
-          />
-          <label htmlFor="cbid3">TON</label>
-          <input
-            type="radio"
-            id="cbid4"
-            name="bid"
-          />
-          <label htmlFor="cbid4">PRM</label>
+          <input type="radio" id="cbid1" name="bid" />
+          <label htmlFor="cbid1" onClick={() => setBidCur("Free")}>
+            Free
+          </label>
+          <input type="radio" id="cbid2" name="bid" />
+          <label htmlFor="cbid2" onClick={() => setBidCur("premium")}>
+            DUR
+          </label>
+          <input type="radio" id="cbid3" name="bid" />
+          <label htmlFor="cbid3" onClick={() => setBidCur("usual")}>
+            Usual
+          </label>
         </div>
         {/* bids */}
         <p>Bids amount</p>
         <ProgressBar
-          values={[0, 1, 10, 100, 500, '1k', '5k', '10k', '50k', '100k']}
+          values={[0, 1, 10, 100, 500, "1k", "5k", "10k", "50k", "100k"]}
+          progress={progress}
+          setProgress={bidChanger}
         />
         {/* Game Type */}
         <p>Type</p>
         <div className="game_type">
-          <input
-            type="radio"
-            id="cgt1"
-            name="game_type"
-          />
-          <label htmlFor="cgt1">
-            Classical <IconArrowDegRight />
+          <input type="radio" id="cgt1" name="game_type" />
+          <label htmlFor="cgt1" onClick={() => setGameType("CLASSIC")}>
+            Classical <IconPlayWhite />
           </label>
-          <input
-            type="radio"
-            id="cgt2"
-            name="game_type"
-          />
-          <label htmlFor="cgt2">
+          <input type="radio" id="cgt2" name="game_type" />
+          <label htmlFor="cgt2" onClick={() => setGameType("PEREVODNOY")}>
             Passing <IconRefresh />
           </label>
-        </div>
-        <div className="cheating">
-          <span className="title">
-            Cheating
-            <IconEye />
-          </span>
-          <div className="chekbox">
-            <input
-              type="checkbox"
-              id="cheat"
-              name="cheating"
-            />
-            <label
-              htmlFor="cheat"
-              className="cheat_label"
-            >
-              <span className="crc"></span>
-            </label>
-          </div>
+          <input type="radio" id="cgt2" name="game_type" />
+          <label htmlFor="cgt2" onClick={() => setGameType("PODKIDNOY")}>
+            Throwing Extra <IconArrowDegRight />
+          </label>
+          <input type="radio" id="cgt2" name="game_type" />
+          <label htmlFor="cgt2" onClick={() => setGameType("SHULLERS")}>
+            With Shullers <IconAlertCircle />
+          </label>
         </div>
       </div>
       <div className="btn_bar">
-        <button className="create_btn">Create game</button>
+        <button className="create_btn" onClick={createGame}>
+          Create game
+        </button>
       </div>
     </LobbiesLayout>
-  )
-}
+  );
+};
 
-export default LobbiesCreate
+export default LobbiesCreate;
