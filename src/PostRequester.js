13af8d6c3cd7fcaffb26b3109b962821aa994499
Esternit <92298236+Esternit@users.noreply.github.com>
2024-09-04 19:46:32 +0300

    started doing sockets

--- src/PostRequester.js
+++ src/PostRequester.js
@@ -4,19 +4,23 @@ import ShowPopup from "./ShowPopup";
 const PostRequester = async (url, body) => {
   try {
     await axios
-    .post(config.url + url, body, {
-      headers: {
-        "Access-Control-Expose-Headers": "X-Session",
-        "X-Session": localStorage.getItem("session_key"),
-      },
-    })
-    .then((res) => {
-      localStorage.setItem("session_key", res.headers.get("X-Session"));
-    });
+      .post(config.url + url, body, {
+        headers: {
+          "Access-Control-Expose-Headers": "X-Session",
+          "X-Session": localStorage.getItem("session_key"),
+        },
+      })
+      .then((res) => {
+        localStorage.setItem("session_key", res.headers.get("X-Session"));
+        console.log(res.data);
+        return res.data;
+      });
   } catch (err) {
     if (err.response.data) {
       ShowPopup(err.response.data, "Error");
     }
+    localStorage.setItem("session_key", err.response.headers.get("X-Session"));
+    return null;
   }
 };
 
---
+++ src/connectToSocket.js
@@ -0,0 +1,21 @@
+import config from "./config";
+import { Stomp } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+
+const connectToSocket = (gameId) => {
+  let stompClient = null;
+  let socket = new SockJS(config.url + "/gameplay");
+  stompClient = Stomp.over(socket);
+  stompClient.connect({}, function (frame) {
+    console.log("connected to the frame: " + frame);
+    stompClient.subscribe(
+      "/topic/game-progress/" + gameId,
+      function (response) {
+        let data = JSON.parse(response.body);
+        console.log(data);
+      }
+    );
+  });
+};
+
+export default connectToSocket;
